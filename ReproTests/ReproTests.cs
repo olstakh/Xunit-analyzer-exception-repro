d79ae1dd19a185ebd8b69533b738e12dba17a8bb
Alex S <olstakh@microsoft.com>
2026-02-13 17:00:55 -0800

    Repro

---
+++ ReproTests/ReproTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+using TestHelper;
+
+namespace ReproTests;
+
+public class ReproTests
+{
+    [Theory]
+    [MemberData(nameof(Class1.Languages), MemberType = typeof(Class1))]
+    public async Task Test(string locale)
+    {
+    }
+}
---
+++ TestHelper/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace TestHelper;
+
+public static class Class1
+{
+    public static TheoryData<string> Languages =>
+        new TheoryData<string>
+            {
+                { "en-US" },
+                { "es-ES" },
+            };
+}
